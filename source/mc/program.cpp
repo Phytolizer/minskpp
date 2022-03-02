1a6eaf59e97aee9b751e924294acf8402ce00a1c
Kyle Coffey <me@phytolizer.dev>
2022-03-01 22:07:49 -0600

    simple repl

--- source/mc/program.cpp
+++ source/mc/program.cpp
@@ -1,5 +1,19 @@
 #include <fmt/format.h>
+#include <iostream>
+#include <string>
 
 int main() {
-    fmt::print("hello world\n");
+    while (true) {
+        fmt::print("> ");
+        std::string line;
+        if (!std::getline(std::cin, line)) {
+            break;
+        }
+
+        if (line == "1 + 2 * 3") {
+            fmt::print("7\n");
+        } else {
+            fmt::print("ERROR: Invalid expression!\n");
+        }
+    }
 }
